eca1d5e3d484a7b12850f0451882230da44702f3
markov vadim <markovadim@gmail.com>
2023-05-11 16:15:56 +0300

    master: create spring boot project

---
+++ news-system-management/src/main/java/ru/clevertec/newssystemmanagement/NewsSystemManagementApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.newssystemmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSystemManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsSystemManagementApplication.class, args);
+    }
+
+}
---
+++ news-system-management/src/test/java/ru/clevertec/newssystemmanagement/NewsSystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.newssystemmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSystemManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
